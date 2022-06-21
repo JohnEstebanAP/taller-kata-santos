979142c19e83c78380a1c30d3988d8b5e7301495
John Esteban Alvarez Piedrahita <esteban.ea145@gmail.com>
2022-06-21 03:56:51 -0500

    Crud Completo para el método ajax

--- crud-ajax/src/index.js
+++ crud-ajax/src/index.js
@@ -1,21 +1,20 @@
-import "@styles/main.css";
-import "@styles/vapor.css";
-import "@assets/db.json";
+/*import "styles/main.css";
+import "styles/vapor.css";
+import "assets/db.json";*/
 
-const  d = document;
+const url1 = "http://localhost:5555/santos/";
+const d = document;
 const container = d.querySelector(".container");
 const table = d.querySelector(".crud-table");
 const from = d.querySelector(".crud-from");
-const title = d.querySelector(".crud-title"); 
+const title = d.querySelector(".crud-title");
 const template = d.querySelector("#crud-template").content;
-
 const fragment = d.createDocumentFragment();
-const xhr = new XMLHttpRequest("xhr");
-var $xhr = d.querySelector(".container");
 
 // pratica de conectivida con ajax 🤓
-(() => {
-
+const ajax = (options) => {
+  let { url, method, llenarData, error, data } = options;
+  const xhr = new XMLHttpRequest();
   xhr.addEventListener("readystatechange", (e) => {
     //para que se ejecute una sola bes y no 4 beses del ciclo de vida
     if (xhr.readyState !== 4) return;
@@ -26,17 +25,16 @@ var $xhr = d.querySelector(".container");
       llenarData(json);
     } else {
       let message = xhr.statusText || "Ocurrió un error";
-      console.log(`Error ${xhr.status}: ${message}`);
-      $xhr.innerHTML =  `Error ${xhr.status}: ${message}`;
+      error(`Error ${xhr.status}: ${message}`);
+      $xhr.innerHTML = `Error ${xhr.status}: ${message}`;
     }
   });
 
-  const url = "http://jsonplaceholder.typicode.com/users";
-  // const url = "assets/db.json";
-  const method = "GET";
   xhr.open(method || "GET", url);
-  xhr.send();
-})();
+  xhr.setRequestHeader("Content-type", "application/json;charset=utf-8");
+  /*combertimos un objeto Json a una cadena de texto js*/
+  xhr.send(JSON.stringify(data));
+};
 
 const llenarData = (res) => {
   console.log(res);
@@ -49,3 +47,103 @@ const llenarData = (res) => {
   // $xhr.appendChild(container);
   // container.querySelector("tbody").appendChild(fragment);
 };
+/**
+ * Metodo para consultar la data de los santos de Athena, y pintarlos en pantalla
+ */
+const getAll = () => {
+  ajax({
+    url: url1,
+    method: "GET",
+    llenarData: (res) => {
+      res.forEach((el) => {
+        template.querySelector(".name").textContent = el.nombre;
+        template.querySelector(".constellation").textContent = el.constelacion;
+        template.querySelector(".edit").dataset.id = el.id;
+        template.querySelector(".edit").dataset.name = el.nombre;
+        template.querySelector(".edit").dataset.constellation = el.constelacion;
+        template.querySelector(".delete").dataset.id = el.id;
+
+        let cloneTemplate = d.importNode(template, true);
+        fragment.appendChild(cloneTemplate);
+      });
+
+      table.querySelector("tbody").appendChild(fragment);
+    },
+    error: (err) => {
+      console.error("Ocurri un error", err);
+      table.insertAdjacentHTML("afterend", `<p><b>${err}</b></p>`);
+    },
+    data: null,
+  });
+};
+
+const addSaint = (event) => {
+  if (event.target === from) {
+    //prevenimos el comportamiento por defecto e un formulario
+    event.preventDefault();
+
+    if (!event.target.id.value) {
+      console.log(
+        "create santo",
+        `nombre: ${event.target.nombre.value}`,
+        ` constelacion: ${event.target.constelacion.value}`
+      );
+      //POST
+      ajax({
+        url: url1,
+        method: "POST",
+        llenarData: (res) => location.reload(),
+        error: () =>
+          from.insertAdjacentHTML("afterend", `<p><b>${err}</b></p>`),
+        data: {
+          nombre: event.target.nombre.value,
+          constelacion: event.target.constelacion.value,
+        },
+      });
+    } else {
+      //PUT
+      ajax({
+        url: url1 + event.target.id.value,
+        method: "PUT",
+        llenarData: (res) => location.reload(),
+        error: () =>
+          from.insertAdjacentHTML("afterend", `<p><b>${err}</b></p>`),
+        data: {
+          nombre: event.target.nombre.value,
+          constelacion: event.target.constelacion.value,
+        },
+      });
+    }
+  }
+};
+
+const editSaint = (event) => {
+  if (event.target.matches(".edit")) {
+    title.textContent = "Editar santo";
+    from.nombre.value = event.target.dataset.name;
+    from.constelacion.value = event.target.dataset.constellation;
+    from.id.value = event.target.dataset.id;
+  }
+
+  if (event.target.matches(".delete")) {
+    let isDelete = confirm(
+      `¿Estás seguro de elimenar el id ${event.target.id}?`
+    );
+
+    if (isDelete) {
+      //DELETE
+      ajax({
+        url: url1 + event.target.dataset.id,
+        method: "DELETE",
+        llenarData: (res) => location.reload(),
+        error: () => alert(err),
+      });
+    }
+  }
+};
+
+d.addEventListener("DOMContentLoaded", getAll);
+
+d.addEventListener("submit", (event) => addSaint(event));
+
+d.addEventListener("click", (event) => editSaint(event));
